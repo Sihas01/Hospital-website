0ed683848a55390b01a012b542670c21013ef1f4
Sihas01 <sihasmandipa77@gmail.com>
2024-09-04 20:39:19 +0530

    change mobile to web with media querys

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+// Get the button
+  let topBtn = document.getElementById("top-btn");
+        
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
