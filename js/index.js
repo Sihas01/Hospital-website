84d0edefd7cf077c9d6c3fbcbc2b644b2930b60e
Sihas01 <sihasmandipa77@gmail.com>
2024-09-07 20:55:52 +0530

    add manifest files

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/Hospital-website/sw.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      })
+      .catch((error) => {
+        console.log('ServiceWorker registration failed: ', error);
+      });
+  });
+}
+
+
+
 // Get the button
   let topBtn = document.getElementById("top-btn");
         
