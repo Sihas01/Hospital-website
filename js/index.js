09aaf98cb219ffe47de06df9af22f1a96a89ab15
Sihas01 <sihasmandipa77@gmail.com>
2024-09-12 23:30:53 +0530

    complete web site

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+//service worker 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/Hospital-website/sw.js')
@@ -14,44 +14,44 @@ if ('serviceWorker' in navigator) {
 
 
 // Get the button
-  let topBtn = document.getElementById("top-btn");
-        
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function() {scrollFunction()};
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        topBtn.style.display = "block";
-    } else {
-        topBtn.style.display = "none";
-    }
+let topBtn = document.getElementById("top-btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
   }
-  
-  // When the user clicks on the button, scroll to the top of the document
-  function toTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function toTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 
-  function openNav() {
-    document.getElementById("mySidenav").style.width = "350px";
-    document.getElementById("mySidenav").style.padding = "10px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0"
-    document.getElementById("mySidenav").style.padding = "0";
-    
-  }
+function openNav() {
+  document.getElementById("mySidenav").style.width = "350px";
+  document.getElementById("mySidenav").style.padding = "10px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0"
+  document.getElementById("mySidenav").style.padding = "0";
+
+}
 
 
-
-  var dropdown = document.getElementsByClassName("dropdown-btn");
+//drop-dwon in nav
+var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
+  dropdown[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
     if (dropdownContent.style.display === "block") {
